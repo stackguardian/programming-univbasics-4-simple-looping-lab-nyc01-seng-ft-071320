cb3d262fb053bae3015e715b641c0c4a743acc05
Muhtasim Ferdous <mferdous98@aol.com>
2020-06-25 22:23:08 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -24,10 +24,10 @@ def output_array(array)
 end
 
 def return_string_array(array)
-  counter = 0 
-  while array[counter] do
-    puts array.to_s 
-    counter += 1 
-  end 
+  count = 0
+  while counter < array.length do
+    array[counter].to_s
+    counter += 1
+  end
 end
   
